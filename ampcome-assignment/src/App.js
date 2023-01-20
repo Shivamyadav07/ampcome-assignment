d9f034d28cee6ec7f48b6de2ba0367bb5173cfe8
Shivamyadav07 <shivamvnsindia@gmail.com>
2023-01-20 21:19:15 +0530

    initial commit in appointment page

--- ampcome-assignment/src/App.js
+++ ampcome-assignment/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Login from './components/login';
+import {Appointments} from './pages/appointments';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Appointments />
     </div>
   );
 }
